956fd4dcc2eb26c1f0eb2b2b6485eec090443d02
Sakib <sjsakib.bd@gmail.com>
2026-03-15 04:56:12 +0600

    embedding io.ReadWriter in socket

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go.sakib.dev/chat
 
 go 1.25.4
 
-require golang.org/x/net v0.52.0 // indirect
+require golang.org/x/net v0.52.0
--- socket.go
+++ socket.go
@@ -1,14 +1,15 @@
 package main
 
-import "golang.org/x/net/websocket"
+import (
+	"io"
+
+	"golang.org/x/net/websocket"
+)
 
 type socket struct {
-	conn *websocket.Conn
+	io.ReadWriter
 	done chan bool
 }
-
-func (s socket) Read(b []byte) (int, error)  { return s.conn.Read(b) }
-func (s socket) Write(b []byte) (int, error) { return s.conn.Write(b) }
 
 func (s socket) Close() error {
 	s.done <- true
@@ -16,7 +17,7 @@ func (s socket) Close() error {
 }
 
 func socketHandler(ws *websocket.Conn) {
-	s := socket{conn: ws, done: make(chan bool)}
+	s := socket{ws, make(chan bool)}
 	go match(s)
 	<-s.done
 }
